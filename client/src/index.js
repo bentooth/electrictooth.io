3551d2b9b25ff9505fc69a841b643794bfedbaa5
Benjamin Vera <ben@soundcontrolrecords.com>
2020-03-15 00:00:04 -0500

    ee

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Root from 'containers/Root'
-import axios from 'axios'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Root from 'containers/Root';
+import axios from 'axios';
 import * as serviceWorker from './serviceWorker';
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL
+axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
-ReactDOM.render(<Root />, document.getElementById('root'))
+ReactDOM.render(<Root />, document.getElementById('root'));
 
 serviceWorker.register();
